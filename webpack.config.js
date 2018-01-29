b097014add12f3221c48260bf4407ebf42590065
lyndonl3091 <lyndonl3091@gmail.com>
2018-01-29 12:22:32 -0500

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,26 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
+    path: path.join(__dirname, '/build'),
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    // loaders: [{
+    //   exclude: /node_modules/,
+    //   loader: 'babel',
+    //   query: {
+    //     presets: ['react', 'es2015', 'stage-1']
+    //   }
+    // }]
+    rules: {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['babel-preset-env', 'babel-preset-react']
       }
-    }]
+    }
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
