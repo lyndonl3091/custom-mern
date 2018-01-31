d082453abb3e773b58fe1ba4a313cf963d9dd4c5
lyndonl3091 <lyndonl3091@gmail.com>
2018-01-30 20:55:43 -0500

    components

---
+++ src/components/splashPage/splashPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const splashPage = (props) => (
+
+  <div>Welcome</div>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -10,27 +10,21 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    // loaders: [{
-    //   exclude: /node_modules/,
-    //   loader: 'babel',
-    //   query: {
-    //     presets: ['react', 'es2015', 'stage-1']
-    //   }
-    // }]
-    rules: {
+    rules: [
       test: /\.js$/,
       exclude: /(node_modules)/,
       loader: 'babel-loader',
       options: {
         presets: ['babel-preset-env', 'babel-preset-react']
       }
-    }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './',
+    hot: true
   }
 };
